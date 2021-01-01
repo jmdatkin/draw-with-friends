4230bf7b450680e4534112eedd21fc99ff6b0fe8
Julian Atkin <jmd.atkin@gmail.com>
2021-01-01 12:19:01 +0000

    added barebones client/server connection code

--- app/src/client.js
+++ app/src/client.js
@@ -1,10 +1,13 @@
 import { WebSocketGameLobbyClient } from 'websocket-game-lobby-client';
 
 const Fictionary = (function() {
-    const gameLobby = new WebSocketGameLobbyClient();
+    const gameLobby = new WebSocketGameLobbyClient({
+        port: 5000
+    });
 
-    
-
-
+    document.getElementById("game-create").addEventListener("click", () => {
+        gameLobby.send('create');
+        console.log(gameLobby);
+    });
 
 })();
--- serv/server.js
+++ serv/server.js
@@ -1,9 +1,29 @@
 const http = require('http-single-serve');
 
-const { WebSocketGameLobbyServer } = require('websocket-game-lobby');
+const { WebSocketGameLobbyServer, EphemeralDataStore } = require('websocket-game-lobby');
 
+//Create datastore
+const datastore = new EphemeralDataStore();
+
+//Create game lobby object
 const gameLobby = new WebSocketGameLobbyServer({
     server: http({
         port: 5000
-    })
+    }),
+    datastore
+});
+
+//Upon game creation
+gameLobby.addEventListener(
+    'create',
+    async ({gameId, playerId}, datastore) => {
+        await datastore.editGame(gameId, async game => {
+            return game;
+        });
+        console.log(`gameId: ${gameId}`);
+    }
+);
+
+gameLobby.addEventListener('message',(d) => {
+    console.log(d);
 });

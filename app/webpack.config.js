6d0e486e4511a4ba4b993424c0ed2f3ca92163dc
Julian Atkin <jmd.atkin@gmail.com>
2021-01-03 01:31:05 +0000

    moved back to webpack structure

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'eval-cheap-module-source-map',
     entry: './src/app.js',
     output: {
         filename: 'bundle.js',

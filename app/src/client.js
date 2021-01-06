b046cf08e1e44934bfbbfd695ca7714bbd5f5aa8
Julian Atkin <jmd.atkin@gmail.com>
2021-01-06 15:08:18 +0000

    improved rendering code to no longer re-draw every stroke per point update; added window onresize evt handler

--- app/src/client.js
+++ app/src/client.js
@@ -1,37 +1,69 @@
 import { WebSocketGameLobbyClient } from 'websocket-game-lobby-client';
+import { v4 as uuidv4 } from 'uuid';
 
 const App = (function () {
     const gameLobby = new WebSocketGameLobbyClient({
         port: 5000
     });
 
+
+    //Returns the squared Euclidean distance
+    const distSq = function(x0,y0,x1,y1) {
+        return (Math.pow(x1-x0,2) + Math.pow(y1-y0,2));
+    }
+
     //Euclidean distance btwn two points
     const dist = function(x0,y0,x1,y1) {
-        return Math.sqrt(Math.pow(x1-x0,2) + Math.pow(y1-y0,2));
+        //return Math.sqrt(Math.pow(x1-x0,2) + Math.pow(y1-y0,2));
+        return Math.sqrt(distSq(x0,y0,x1,y1));
     };
 
     const Canvas = (function () {
         //DOM references
-        const canvasEl = document.getElementById("main-canv");
-        const ctx = canvasEl.getContext("2d");
+        const mainCanvas = document.getElementById("main-canv");
+        const auxCanvas = document.getElementById("aux-canv");
+        const canvasWrapper = document.querySelector(".canvas-wrapper");
+
+        const mainCtx = mainCanvas.getContext("2d");
 
         //Static constants
         const width = 1600;
         const height = 900;
 
-        const setCanvasSize = function (w, h) {
-            canvasEl.setAttribute("width", w);
-            canvasEl.setAttribute("height", h);
+        // const offsetLeft = mainCanvas.offsetLeft;
+        // const offsetTop = mainCanvas.offsetTop;
+
+        var offsetLeft = canvasWrapper.getBoundingClientRect().left;
+        var offsetTop = canvasWrapper.getBoundingClientRect().top;
+
+        //Binding offset vars to window resize
+        window.onresize = () => {
+            offsetLeft = canvasWrapper.getBoundingClientRect().left;
+            offsetTop = canvasWrapper.getBoundingClientRect().top;
         };
 
+        const setElementDims = function(el,w,h) {
+            el.setAttribute("width", w);
+            el.setAttribute("height", h)
+        }
+
+        // const setCanvasSize = function (w, h) {
+        //     canvasEl.setAttribute("width", w);
+        //     canvasEl.setAttribute("height", h);
+        // };
+
+        //Initialize canvas properties
         const init = function () {
-            setCanvasSize(width, height);
-            ctx.lineWidth = 10;
-            ctx.lineCap = 'round';
-        };
+            //setCanvasSize(width, height);
+            setElementDims(mainCanvas,width,height);
+            setElementDims(auxCanvas,width,height);
+            
+            canvasWrapper.style.width = `${width}px`;
+            canvasWrapper.style.height = `${height}px`;
 
-        const offsetLeft = canvasEl.offsetLeft;
-        const offsetTop = canvasEl.offsetTop;
+            mainCtx.lineWidth = 10;
+            mainCtx.lineCap = 'round';
+        };
 
         //Page coord to canvas coord conversion
         const ptoc = function(c) {
@@ -41,7 +73,12 @@ const App = (function () {
             };
         };
 
+
+        //Module for managing data points for brush strokes
         const DrawData = (function () {
+
+            /*  POINT */
+            //Represents a single point to be drawn
             function Point(x, y) {
                 this.x = x;
                 this.y = y;
@@ -52,10 +89,13 @@ const App = (function () {
             Point.prototype.setNext = function(next) {
                 this.next = next;
             };
+            /*  END POINT */
 
 
+            /*  STROKE */
             function Stroke() {
                 this.points = [];
+                this.id = uuidv4();
             }
 
             Stroke.prototype.addPoint = function(point) {
@@ -74,7 +114,12 @@ const App = (function () {
 
             Stroke.prototype.getFirstPoint = function() {
                 return (this.points.length > 0) ? this.points[0] : null;
-            }
+            };
+
+            Stroke.prototype.getID = function() {
+                return this.id;
+            };
+            /* END STROKE */
 
             const strokes = [];
 
@@ -107,6 +152,7 @@ const App = (function () {
             
             //Mouse pos differential to trigger new point added
             const delta = 4;
+            const deltaSq = delta*delta;
 
             const mouseDownHandler = function(e) {
                 currentStroke = new DrawData.Stroke();
@@ -115,39 +161,41 @@ const App = (function () {
                 let newPoint = new DrawData.Point(coords.x,coords.y);
                 currentStroke.addPoint(newPoint);
 
-                canvasEl.addEventListener("mousemove", mouseMoveHandler);
+                mainCanvas.addEventListener("mousemove", mouseMoveHandler);
             }
 
             const mouseMoveHandler = function(e) {
                 let coords = ptoc({x: e.pageX,  y: e.pageY});
                 if (strokeInProgress && currentStroke !== null) {
                     let latestPoint = currentStroke.getLatestPoint();
-                    if (dist(coords.x, coords.y, latestPoint.x, latestPoint.y) > delta
+                    //if (dist(coords.x, coords.y, latestPoint.x, latestPoint.y) > delta
+                    if (distSq(coords.x, coords.y, latestPoint.x, latestPoint.y) > deltaSq
                         || latestPoint === null) {
                         let currentPoint = new DrawData.Point(coords.x,coords.y);
                         currentStroke.addPoint(currentPoint);
                         
-                        Renderer.clear();
-                        Renderer.drawStroke(currentStroke);
+                        //Renderer.clear();
+                        //Renderer.drawStroke(currentStroke);
+                        Renderer.drawFromPoint(latestPoint);
                     }
                 }
             }
 
             const mouseUpHandler = function() {
                 if (strokeInProgress) {
-                    canvasEl.removeEventListener("mousemove", mouseMoveHandler);
+                    mainCanvas.removeEventListener("mousemove", mouseMoveHandler);
                     strokeInProgress = false;
                     DrawData.addStroke(currentStroke);
                     strokeInProgress = null;
 
                     DrawData.printStrokes();    //Testing
-                    Renderer.drawStroke(currentStroke);
+                    //Renderer.drawStroke(currentStroke);
                 }
             }
 
             return function() {
-                canvasEl.addEventListener("mousedown", mouseDownHandler);
-                canvasEl.addEventListener("mouseup", mouseUpHandler);
+                mainCanvas.addEventListener("mousedown", mouseDownHandler);
+                mainCanvas.addEventListener("mouseup", mouseUpHandler);
             };
         })();
         bindEventListeners();
@@ -157,40 +205,66 @@ const App = (function () {
             const drawStroke = function(stroke) {
                 if (stroke instanceof DrawData.Stroke) {
                     let currentPoint = stroke.getFirstPoint();
-                    ctx.moveTo(currentPoint.x,currentPoint.y);
+                    mainCtx.moveTo(currentPoint.x,currentPoint.y);
                     do {
                         currentPoint = currentPoint.next;
-                        ctx.lineTo(currentPoint.x,currentPoint.y);
+                        mainCtx.lineTo(currentPoint.x,currentPoint.y);
                     }
                     while (currentPoint.next !== null);
-                    ctx.stroke();
+                    mainCtx.stroke();
                 }
             };
 
-            const clear = function() {
-                ctx.clearRect(0,0,width,height);
+            const drawFromPoint = function(point) {
+                if (point instanceof DrawData.Point) {
+                    let currentPoint = point;
+                    mainCtx.moveTo(currentPoint.x,currentPoint.y);
+                    do {
+                        currentPoint = currentPoint.next;
+                        mainCtx.lineTo(currentPoint.x,currentPoint.y);
+                    }
+                    while (currentPoint.next !== null);
+                    mainCtx.stroke();
+                    return currentPoint;
+                }
+                return null;
             }
 
+            const updateStroke = function(stroke) {
+                if (stroke instanceof DrawData.Stroke)
+                    strokesInProgess[stroke.id] = stroke.getLatestPoint();
+            };
+
+            const clear = function() {
+                mainCtx.clearRect(0,0,width,height);
+            }
+
+            const strokesInProgress = {};
 
             //Represents a stroke in progress, with data expected to be arriving over time
-            function StrokeStream(stroke) {
+            /*function StrokeStream(stroke) {
                 this.boundStroke = null;
-                if (typeof stroke !== 'undefined')
+                this.lastPoint = null;
+                if (typeof stroke !== 'undefined' && stroke instanceof DrawData.Stroke) {
                     this.boundStroke = stroke;
+                    this.lastPoint = stroke.getLatestPoint();
+                }
             }
 
             StrokeStream.prototype.bindStroke = function(stroke) {
                 this.boundStroke = stroke;
+                this.lastPoint = stroke.getLatestPoint();
             }
 
-            StrokeStream.prototype.update = function(stroke) {
+            StrokeStream.prototype.update = function() {
                 
             }
 
-            let strokeStreams = [];
+            let strokeStreams = [];*/
 
             const Renderer_exported = {
                 drawStroke: drawStroke,
+                drawFromPoint: drawFromPoint,
                 clear: clear
             };
             
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devtool: 'eval-cheap-source-map',
     module: {
         rules: [
             {
